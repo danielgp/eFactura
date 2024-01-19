64f1fd3b9ed757b2169d7e174ae907b5a3007ada
Daniel Popiniuc <danielpopiniuc@gmail.com>
2024-01-19 17:43:55 +0200

    cod reorg for Comments assesment

--- source/ElectronicInvoiceComments.php
+++ source/ElectronicInvoiceComments.php
@@ -9,7 +9,7 @@
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
@@ -37,19 +37,34 @@ class ElectronicInvoiceComments
     {
         $arrayCommentsRaw      = $this->getCommentsFromFileAsArray();
         $arrayCommentsForTable = [];
+        $arrayMapping          = [
+            'BG-' => 'Group',
+            'BT-' => 'Field',
+        ];
         foreach ($arrayCommentsRaw as $key => $value) {
-            $arrayCommentsForTable[] = [
-                'ID'               => $key,
-                'Tag'              => $value['HierarchycalTagName'],
-                'Level'            => $value['Level'],
-                'Cardinality'      => $value['Cardinality'],
-                'OperationalTerm'  => $value['OperationalTerm'],
-                'Description_RO'   => $value['Description']['ro_RO'],
-                'Description_EN'   => (array_key_exists('en_US', $value['Description']) ? $value['Description']['en_US'] : '-'),
-                'UsageNote'        => (array_key_exists('UsageNote', $value) ? $value['UsageNote'] : '-'),
-                'RequirementID'    => $value['RequirementID'],
-                'SemanticDataType' => (array_key_exists('SemanticDataType', $value) ? $value['SemanticDataType'] : '-'),
+            $strKeyPrefix = substr($key, 0, 3);
+            $strIdType    = (in_array($strKeyPrefix, array_keys($arrayMapping)) ? $arrayMapping[$strKeyPrefix] : '?');
+            $arrayTemp    = [
+                'ID'                 => $key,
+                'IT_Type'            => $strIdType,
+                'Level'              => $value['Level'],
+                'Cardinality'        => $value['Cardinality'],
+                'OperationalTerm_EN' => $this->getKeyFromArrayOrAlternative('en_US', $value['OperationalTerm'], '-'),
+                'OperationalTerm_RO' => $this->getKeyFromArrayOrAlternative('ro_RO', $value['OperationalTerm'], '-'),
+                'Description_EN'     => $this->getKeyFromArrayOrAlternative('en_US', $value['Description'], '-'),
+                'Description_RO'     => $this->getKeyFromArrayOrAlternative('ro_RO', $value['Description'], '-'),
+                'UsageNote_EN'       => $this->getKeyFromArrayOrAlternative('en_US', $value['UsageNote'], $value, '-'),
+                'UsageNote_RO'       => $this->getKeyFromArrayOrAlternative('ro_RO', $value['UsageNote'], $value, '-'),
+                'RequirementID'      => $value['RequirementID'],
+                'SemanticDataType'   => $this->getKeyFromArrayOrAlternative('SemanticDataType', $value, '-'),
             ];
+            if (is_array($value['HierarchycalTagName'])) {
+                foreach ($value['HierarchycalTagName'] as $value2) {
+                    $arrayCommentsForTable[] = array_merge(['Tag' => $value2], $arrayTemp);
+                }
+            } else {
+                $arrayCommentsForTable[] = array_merge(['Tag' => $value['HierarchycalTagName']], $arrayTemp);
+            }
         }
         return $arrayCommentsForTable;
     }
@@ -66,5 +81,14 @@ class ElectronicInvoiceComments
             }
         }
         return $arrayCommentsForTable;
+    }
+
+    public function getKeyFromArrayOrAlternative(string $strKey, array $arrayIn, string $strAlternative): string
+    {
+        $strToReturn = $strAlternative;
+        if (array_key_exists($strKey, $arrayIn)) {
+            $strToReturn = $arrayIn[$strKey];
+        }
+        return $strToReturn;
     }
 }
